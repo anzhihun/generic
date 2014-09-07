066e21ad3914dc3da62bdd1e9d118bc24661d5e2
worge ling <anzhihun@gmail.com>
2014-09-07 19:39:01 +0800

    Add Remove and QuickSort function of any type slice

---
+++ slice.go
@@ -0,0 +1,262 @@
+package generic
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+type slice struct {
+	slicePtr interface{}
+}
+
+// New a slice with slice ptr
+func Slice(slicePtr interface{}) *slice {
+	return &slice{slicePtr}
+}
+
+// Remove element at index of slice
+func (s *slice) Remove(index int) error {
+	err := s.checkSlice()
+	if err != nil {
+		return err
+	}
+
+	slicePtrValue := reflect.ValueOf(s.slicePtr)
+	sliceValue := slicePtrValue.Elem()
+	if index < 0 || index >= sliceValue.Len() {
+		return errors.New("index out of range!")
+	}
+	sliceValue.Set(reflect.AppendSlice(sliceValue.Slice(0, index), sliceValue.Slice(index+1, sliceValue.Len())))
+	return nil
+}
+
+func (s *slice) checkSlice() error {
+	if s.slicePtr == nil {
+		return errors.New("slice is nil!")
+	}
+
+	slicePtrValue := reflect.ValueOf(s.slicePtr)
+	// should be pointer
+	if slicePtrValue.Type().Kind() != reflect.Ptr {
+		return errors.New("should be slice pointer!")
+	}
+
+	sliceValue := slicePtrValue.Elem()
+	// should be slice
+	if sliceValue.Type().Kind() != reflect.Slice {
+		return errors.New("should be slice pointer!")
+	}
+
+	return nil
+}
+
+// sort slice by quick sort algorithm
+// support slice of all int and uint types
+// and support stuct which has the compare function, function name should be "Compare", and return int. such as
+// type student stuct {
+// 	age int
+// }
+//
+// // return value:
+// // if value == 0, equal with other student,
+// // if value < 0, less than other,
+// // if value > 0, greater than other student.
+// func (s student) Compare(other student) int {
+// 	return s.age - other.age
+// }
+//
+func (s *slice) QuickSort() error {
+	err := s.checkSlice()
+	if err != nil {
+		return err
+	}
+
+	slicePtrValue := reflect.ValueOf(s.slicePtr)
+	sliceValue := slicePtrValue.Elem()
+	if sliceValue.Len() <= 1 {
+		return nil
+	}
+	compareFuncName := "Compare"
+	err = checkTypeOfSort(sliceValue.Index(0), compareFuncName)
+	if err != nil {
+		return err
+	}
+	quickSort(sliceValue, 0, sliceValue.Len()-1, compareFuncName)
+	return nil
+}
+
+// if the type is not supported, or the struct doesn't have compare function, then return error.
+func checkTypeOfSort(elem reflect.Value, funcName string) error {
+	switch elem.Type().Kind() {
+	case reflect.Struct:
+		funcValue := elem.MethodByName(funcName)
+		if funcValue.IsNil() {
+			return errors.New("no compare function!")
+		}
+		if !strings.HasSuffix(funcValue.Type().String(), "int") {
+			return errors.New("compare function should return int!")
+		}
+		break
+	case reflect.Int8:
+		fallthrough
+	case reflect.Int16:
+		fallthrough
+	case reflect.Int32:
+		fallthrough
+	case reflect.Int64:
+		fallthrough
+	case reflect.Int:
+		fallthrough
+	case reflect.Uint:
+		fallthrough
+	case reflect.Uint8:
+		fallthrough
+	case reflect.Uint16:
+		fallthrough
+	case reflect.Uint32:
+		fallthrough
+	case reflect.Uint64:
+		break
+	default:
+		fmt.Println(elem.Type().Kind())
+		return errors.New("unsupport type: " + elem.Type().Kind().String())
+	}
+
+	return nil
+}
+
+// Basicly it is same as QuickSort function.
+// It just give you choice to decide the compare function which is used by struct
+func (s *slice) QuickSortBy(compareFuncName string) error {
+	err := s.checkSlice()
+	if err != nil {
+		return err
+	}
+
+	slicePtrValue := reflect.ValueOf(s.slicePtr)
+	sliceValue := slicePtrValue.Elem()
+	if sliceValue.Len() <= 1 {
+		return nil
+	}
+	if err = checkTypeOfSort(sliceValue.Index(0), compareFuncName); err != nil {
+		return err
+	}
+
+	quickSort(sliceValue, 0, sliceValue.Len()-1, compareFuncName)
+	return nil
+}
+
+// the internal function for implementing quick sort algorithm.
+func quickSort(slice reflect.Value, lowIndex, highIndex int, compareFuncName string) {
+	if lowIndex < 0 {
+		lowIndex = 0
+	}
+	if highIndex > slice.Len()-1 {
+		highIndex = slice.Len() - 1
+	}
+	if lowIndex >= highIndex {
+		return
+	}
+
+	firstIndex := lowIndex
+	lastIndex := highIndex
+	key := clone(slice.Index(firstIndex))
+
+	for firstIndex < lastIndex {
+
+		for firstIndex < lastIndex && compare(slice.Index(lastIndex), key, compareFuncName) >= 0 {
+			lastIndex = lastIndex - 1
+		}
+		swap(slice, firstIndex, lastIndex)
+
+		for firstIndex < lastIndex && compare(slice.Index(firstIndex), key, compareFuncName) <= 0 {
+			firstIndex = firstIndex + 1
+		}
+		swap(slice, firstIndex, lastIndex)
+	}
+
+	quickSort(slice, lowIndex, firstIndex-1, compareFuncName)
+	quickSort(slice, lastIndex+1, highIndex, compareFuncName)
+}
+
+func clone(value reflect.Value) reflect.Value {
+	newValue := reflect.New(value.Type()).Elem()
+	newValue.Set(value)
+	return newValue
+}
+
+// compare two elements of the slice
+// return value:
+// if value == 0, va1 is equal to val2,
+// if value < 0, va1 is less than val2,
+// if value > 0, va1 is greater than val2.
+func compare(val1, val2 reflect.Value, compareFuncName string) int {
+
+	switch val1.Type().Kind() {
+	case reflect.Int8:
+		return int(val1.Interface().(int8)) - int(val2.Interface().(int8))
+		break
+	case reflect.Int16:
+		return int(val1.Interface().(int16)) - int(val2.Interface().(int16))
+		break
+	case reflect.Int32:
+		return int(val1.Interface().(int32)) - int(val2.Interface().(int32))
+		break
+	case reflect.Int64:
+		v1 := val1.Interface().(int64)
+		v2 := val2.Interface().(int64)
+		if v1 < v2 {
+			return -1
+		} else if v1 == v2 {
+			return 0
+		} else {
+			return 1
+		}
+		break
+
+	case reflect.Int:
+		return val1.Interface().(int) - val2.Interface().(int)
+		break
+	case reflect.Uint:
+		return int(val1.Interface().(uint)) - int(val2.Interface().(uint))
+		break
+	case reflect.Uint8:
+		return int(val1.Interface().(uint8)) - int(val2.Interface().(uint8))
+		break
+	case reflect.Uint16:
+		return int(val1.Interface().(uint16)) - int(val2.Interface().(uint16))
+		break
+	case reflect.Uint32:
+		return int(val1.Interface().(uint32)) - int(val2.Interface().(uint32))
+		break
+	case reflect.Uint64:
+		v1 := val1.Interface().(uint64)
+		v2 := val2.Interface().(uint64)
+		if v1 < v2 {
+			return -1
+		} else if v1 == v2 {
+			return 0
+		} else {
+			return 1
+		}
+		break
+	default:
+		compareFuncValue := val1.MethodByName(compareFuncName)
+		return compareFuncValue.Call([]reflect.Value{val2})[0].Interface().(int)
+	}
+
+	return 0
+}
+
+// swap two elements of the slice
+func swap(sliceValue reflect.Value, index1, index2 int) {
+	if index1 == index2 {
+		return
+	}
+
+	cache := clone(sliceValue.Index(index1))
+	sliceValue.Index(index1).Set(sliceValue.Index(index2))
+	sliceValue.Index(index2).Set(cache)
+}
---
+++ slice_test.go
@@ -0,0 +1,415 @@
+package generic
+
+import (
+	"strconv"
+	"testing"
+)
+
+type student struct {
+	name string
+	age  int
+}
+
+func (s student) Compare(other student) int {
+	if s.age < other.age {
+		return -1
+	} else if s.age == other.age {
+		return 0
+	} else {
+		return 1
+	}
+}
+
+func TestSliceRemove(t *testing.T) {
+	values := []byte{1, 2, 3}
+	err := Slice(&values).Remove(0)
+	if err != nil || len(values) != 2 || values[0] != 2 || values[1] != 3 {
+		t.Fatal("Failed to remove first item!")
+	}
+
+	values2 := []byte{1, 2, 3}
+	err = Slice(&values2).Remove(2)
+	if err != nil || len(values2) != 2 || values2[0] != 1 || values2[1] != 2 {
+		t.Fatal("Failed to remove last item!")
+	}
+
+	values3 := []byte{1, 2, 3}
+	err = Slice(&values3).Remove(1)
+	if err != nil || len(values3) != 2 || values3[0] != 1 || values3[1] != 3 {
+		t.Fatal("Failed to remove middle item!")
+	}
+
+	values4 := []byte{1, 2, 3}
+	err = Slice(&values4).Remove(3)
+	if err == nil {
+		t.Fatal("It should be error when removing out of range item!")
+	}
+
+	values5 := []byte{1, 2, 3}
+	err = Slice(values5).Remove(1)
+	if err == nil {
+		t.Fatal("It should be error when the parameter is slice!")
+	}
+
+	values6 := [3]byte{1, 2, 3}
+	err = Slice(&values6).Remove(1)
+	if err == nil {
+		t.Fatal("It should be error when the parameter is array pointer!")
+	}
+
+	values7 := string("test")
+	err = Slice(&values7).Remove(1)
+	if err == nil {
+		t.Fatal("It should be error when the parameter is string!")
+	}
+}
+
+func TestSliceQuickSort_Struct(t *testing.T) {
+	students := []student{}
+	err := Slice(&students).QuickSort()
+	if err != nil {
+		t.Fatal("Failed to quick sort empty slice! error: ", err)
+	}
+
+	students = append(students, student{name: "1", age: 100})
+	if err = Slice(&students).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort slice contains only 1 element! error: ", err)
+	}
+	if len(students) != 1 || students[0].age != 100 {
+		t.Fatal("After quick sort slice contains only 1 element, the element should be right!")
+	}
+
+	students = append(students, student{name: "2", age: 12})
+	if err = Slice(&students).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort slice contains only 2 element! error: ", err)
+	}
+	if len(students) != 2 || students[0].age != 12 || students[1].age != 100 {
+		t.Fatal("After quick sort slice contains only 2 element, the elements should be right and ordered!")
+	}
+
+	students = append(students, student{name: "3", age: 15})
+	students = append(students, student{name: "4", age: 14})
+	students = append(students, student{name: "5", age: 11})
+	students = append(students, student{name: "6", age: 22})
+	students = append(students, student{name: "7", age: 4})
+
+	if err = Slice(&students).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort slice contains many elements! error: ", err)
+	}
+	for i := 0; i < len(students)-2; i++ {
+		if students[i].age > students[i+1].age {
+			t.Fatal("After quick sort slice contains many elements, the elements should be right and ordered! actual: ", students)
+		}
+	}
+
+	// ordered slice
+	students2 := []student{}
+	for i := 0; i < 1000; i++ {
+		students2 = append(students2, student{name: strconv.Itoa(i), age: i})
+	}
+	if err = Slice(&students2).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort ordered slice! error: ", err)
+	}
+
+	for i := 0; i < 1000; i++ {
+		if students2[i].age != i {
+			t.Fatal("After quick sort ordered slice, the elements should be right and ordered! actual: ", students2)
+		}
+	}
+
+	students3 := []student{}
+	for i := 999; i >= 0; i-- {
+		students3 = append(students3, student{name: strconv.Itoa(i), age: i})
+	}
+	if err = Slice(&students3).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort reverse order slice! error: ", err)
+	}
+
+	count := len(students)
+	for i := 0; i < count; i++ {
+		if students3[i].age != i {
+			t.Fatal("After quick sort reverse order slice, the elements should be right and ordered! actual: ", students3)
+		}
+	}
+
+	students4 := []student{}
+	students4 = append(students4, student{name: strconv.Itoa(10), age: 10})
+	students4 = append(students4, student{name: strconv.Itoa(11), age: 9})
+	students4 = append(students4, student{name: strconv.Itoa(12), age: 9})
+	students4 = append(students4, student{name: strconv.Itoa(13), age: 8})
+	students4 = append(students4, student{name: strconv.Itoa(14), age: 10})
+	if err = Slice(&students4).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort slice contains repeat elements! error:", err)
+	}
+
+	count = len(students4)
+	for i := 0; i < count-1; i++ {
+		if students4[i].age > students4[i+1].age {
+			t.Fatal("After quick sort slice contains repeat elements, the elements should be right and ordered! actual: ", students4)
+		}
+	}
+}
+
+func TestSliceQuickSort_Int8(t *testing.T) {
+	testInt8s := []int8{}
+	err := Slice(&testInt8s).QuickSort()
+	if err != nil {
+		t.Fatal("Quick sort should support int8 slice! error: ", err)
+	}
+
+	testInt8s = append(testInt8s, 10)
+	if err = Slice(&testInt8s).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort int8 slice contains with 1 element! error: ", err)
+	}
+	if len(testInt8s) != 1 || testInt8s[0] != 10 {
+		t.Fatal("After quick sort int8 slice contains 1 element, the element should be right and ordered!")
+	}
+
+	testInt8s = append(testInt8s, 1)
+	if err = Slice(&testInt8s).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort int8 slice contains 2 element! error: ", err)
+	}
+	if len(testInt8s) != 2 || testInt8s[0] != 1 || testInt8s[1] != 10 {
+		t.Fatal("After quick sort int8 slice contains 2 element, the elements should be right and ordered!")
+	}
+
+	moreInt8s := []int8{}
+	var index int8 = 0
+	for index = 126; index >= -127; index-- {
+		moreInt8s = append(moreInt8s, index)
+	}
+	if err = Slice(&moreInt8s).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort int8 slice contains many reverse order elements! error: ", err)
+	}
+	count := len(moreInt8s)
+	if count != 254 {
+		t.Fatal("After quick sort int8 slice contains many reverse order elements, the element count should be right!")
+	}
+
+	var i int
+	for index = -127; index < 127; index++ {
+		i = int(index) + 127
+		if moreInt8s[i] != index {
+			t.Fatal("After quick sort int8 slice contains many reverse order elements, the elements should be right and ordered!")
+		}
+	}
+}
+
+func TestSliceQuickSort_Int16(t *testing.T) {
+	testint16s := []int16{}
+	err := Slice(&testint16s).QuickSort()
+	if err != nil {
+		t.Fatal("Quick sort should support int16 slice! error: ", err)
+	}
+
+	testint16s = append(testint16s, 30000)
+	if err = Slice(&testint16s).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort int16 slice contains 1 element! error: ", err)
+	}
+	if len(testint16s) != 1 || testint16s[0] != 30000 {
+		t.Fatal("After quick sort int16 slice contains 1 element, the elements should be right and ordered!")
+	}
+
+	testint16s = append(testint16s, -30000)
+	if err = Slice(&testint16s).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort int16 slice contains 2 element! error: ", err)
+	}
+	if len(testint16s) != 2 || testint16s[0] != -30000 || testint16s[1] != 30000 {
+		t.Fatal("After quick sort int16 slice contains 2 element, the elements should be right and ordered!")
+	}
+}
+
+func TestSliceQuickSort_Int32(t *testing.T) {
+	testint32s := []int32{}
+	err := Slice(&testint32s).QuickSort()
+	if err != nil {
+		t.Fatal("Quick sort should support int32 slice! error: ", err)
+	}
+
+	testint32s = append(testint32s, 2000000000)
+	if err = Slice(&testint32s).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort int32 slice contains 1 element! error: ", err)
+	}
+	if len(testint32s) != 1 || testint32s[0] != 2000000000 {
+		t.Fatal("After quick sort int32 slice contains 1 element, the elements should be right and ordered!")
+	}
+
+	testint32s = append(testint32s, -2000000000)
+	if err = Slice(&testint32s).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort int32 slice contains 2 element! error: ", err)
+	}
+	if len(testint32s) != 2 || testint32s[0] != -2000000000 || testint32s[1] != 2000000000 {
+		t.Fatal("After quick sort int32 slice contains 2 element, the elements should be right and ordered!")
+	}
+}
+
+func TestSliceQuickSort_Int64(t *testing.T) {
+	testint64s := []int64{}
+	err := Slice(&testint64s).QuickSort()
+	if err != nil {
+		t.Fatal("Quick sort should support int64 slice! error: ", err)
+	}
+
+	testint64s = append(testint64s, 6000000000)
+	if err = Slice(&testint64s).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort int64 slice contains 1 element! error: ", err)
+	}
+	if len(testint64s) != 1 || testint64s[0] != 6000000000 {
+		t.Fatal("After quick sort int64 slice contains 1 element, the elements should be right and ordered!")
+	}
+
+	testint64s = append(testint64s, -6000000000)
+	if err = Slice(&testint64s).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort int64 slice contains 2 element! error: ", err)
+	}
+	if len(testint64s) != 2 || testint64s[0] != -6000000000 || testint64s[1] != 6000000000 {
+		t.Fatal("After quick sort int64 slice contains 2 element, the elements should be right and ordered!")
+	}
+}
+
+func TestSliceQuickSort_Int(t *testing.T) {
+	testints := []int{}
+	err := Slice(&testints).QuickSort()
+	if err != nil {
+		t.Fatal("Quick sort should support int slice! error: ", err)
+	}
+
+	testints = append(testints, 75536)
+	if err = Slice(&testints).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort int slice contains 1 element! error: ", err)
+	}
+	if len(testints) != 1 || testints[0] != 75536 {
+		t.Fatal("After quick sort int slice contains 1 element, the elements should be right and ordered!")
+	}
+
+	testints = append(testints, 65536)
+	if err = Slice(&testints).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort int slice contains 2 element! error: ", err)
+	}
+	if len(testints) != 2 || testints[0] != 65536 || testints[1] != 75536 {
+		t.Fatal("After quick sort int slice contains 2 element, the elements should be right and ordered!")
+	}
+}
+
+func TestSliceQuickSort_Uint(t *testing.T) {
+	testuints := []uint{}
+	err := Slice(&testuints).QuickSort()
+	if err != nil {
+		t.Fatal("Quick sort should support uint slice! error: ", err)
+	}
+
+	testuints = append(testuints, 10)
+	if err = Slice(&testuints).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort uint slice contains 1 element! error: ", err)
+	}
+	if len(testuints) != 1 || testuints[0] != 10 {
+		t.Fatal("After quick sort uint slice contains 1 element, the elements should be right and ordered!")
+	}
+
+	testuints = append(testuints, 1)
+	if err = Slice(&testuints).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort uint slice contains 2 element! error: ", err)
+	}
+	if len(testuints) != 2 || testuints[0] != 1 || testuints[1] != 10 {
+		t.Fatal("After quick sort uint slice contains 2 element, the elements should be right and ordered!")
+	}
+}
+
+func TestSliceQuickSort_Uint8(t *testing.T) {
+	testuint8s := []uint8{}
+	err := Slice(&testuint8s).QuickSort()
+	if err != nil {
+		t.Fatal("Quick sort should support uint8 slice! error: ", err)
+	}
+
+	testuint8s = append(testuint8s, 255)
+	if err = Slice(&testuint8s).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort uint8 slice contains 1 element! error: ", err)
+	}
+	if len(testuint8s) != 1 || testuint8s[0] != 255 {
+		t.Fatal("After quick sort uint8 slice contains 1 element, the elements should be right and ordered!")
+	}
+
+	testuint8s = append(testuint8s, 128)
+	if err = Slice(&testuint8s).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort uint8 slice contains 2 element! error: ", err)
+	}
+
+	if len(testuint8s) != 2 || testuint8s[0] != 128 || testuint8s[1] != 255 {
+		t.Fatal("After quick sort uint8 slice contains 2 element, the elements should be right and ordered!")
+	}
+}
+
+func TestSliceQuickSort_Uint16(t *testing.T) {
+	testuint16s := []uint16{}
+	err := Slice(&testuint16s).QuickSort()
+	if err != nil {
+		t.Fatal("Quick sort should support uint16 slice! error: ", err)
+	}
+
+	testuint16s = append(testuint16s, 65535)
+	if err = Slice(&testuint16s).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort uint16 slice contains 1 element! error: ", err)
+	}
+	if len(testuint16s) != 1 || testuint16s[0] != 65535 {
+		t.Fatal("After quick sort uint16 slice contains 1 element, the elements should be right and ordered!")
+	}
+
+	testuint16s = append(testuint16s, 15535)
+	if err = Slice(&testuint16s).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort uint16 slice contains 2 element! error: ", err)
+	}
+
+	if len(testuint16s) != 2 || testuint16s[0] != 15535 || testuint16s[1] != 65535 {
+		t.Fatal("After quick sort uint16 slice contains 2 element, the elements should be right and ordered!")
+	}
+}
+
+func TestSliceQuickSort_Uint32(t *testing.T) {
+	testuint32s := []uint32{}
+	err := Slice(&testuint32s).QuickSort()
+	if err != nil {
+		t.Fatal("Quick sort should support uint32 slice! error: ", err)
+	}
+
+	testuint32s = append(testuint32s, 4000000000)
+	if err = Slice(&testuint32s).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort uint32 slice contains 1 element! error: ", err)
+	}
+	if len(testuint32s) != 1 || testuint32s[0] != 4000000000 {
+		t.Fatal("After quick sort uint32 slice contains 1 element, the elements should be right and ordered!")
+	}
+
+	testuint32s = append(testuint32s, 1000000000)
+	if err = Slice(&testuint32s).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort uint32 slice contains 2 element! error: ", err)
+	}
+
+	if len(testuint32s) != 2 || testuint32s[0] != 1000000000 || testuint32s[1] != 4000000000 {
+		t.Fatal("After quick sort uint32 slice contains 2 element, the elements should be right and ordered!")
+	}
+}
+
+func TestSliceQuickSort_Uint64(t *testing.T) {
+	testuint64s := []uint64{}
+	err := Slice(&testuint64s).QuickSort()
+	if err != nil {
+		t.Fatal("Quick sort should support uint64 slice! error: ", err)
+	}
+
+	testuint64s = append(testuint64s, 10000000001000000000)
+	if err = Slice(&testuint64s).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort uint64 slice contains 1 element! error: ", err)
+	}
+	if len(testuint64s) != 1 || testuint64s[0] != 10000000001000000000 {
+		t.Fatal("After quick sort uint64 slice contains 1 element, the elements should be right and ordered!")
+	}
+
+	testuint64s = append(testuint64s, 10000000000000000000)
+	if err = Slice(&testuint64s).QuickSort(); err != nil {
+		t.Fatal("Failed to quick sort uint64 slice contains 2 element! error: ", err)
+	}
+	if len(testuint64s) != 2 || testuint64s[0] != 10000000000000000000 || testuint64s[1] != 10000000001000000000 {
+		t.Fatal("After quick sort uint64 slice contains 2 element, the elements should be right and ordered!")
+	}
+}
